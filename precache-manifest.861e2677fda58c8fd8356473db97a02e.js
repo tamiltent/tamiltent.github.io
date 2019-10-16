128562c69bd4020539bf302c973ce23ec378a488
Mani <kodebot@hotmail.com>
2019-10-16 23:41:47 +0100

    initial commit

---
+++ precache-manifest.861e2677fda58c8fd8356473db97a02e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414005209e050199e33f4f46f2b74754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7078e483306eba0a061",
+    "url": "/static/css/main.915947c3.chunk.css"
+  },
+  {
+    "revision": "6d879babebbe98215361",
+    "url": "/static/js/2.ae810050.chunk.js"
+  },
+  {
+    "revision": "c7078e483306eba0a061",
+    "url": "/static/js/main.a42109c6.chunk.js"
+  },
+  {
+    "revision": "57b5e7dd3ab857bb0b3b",
+    "url": "/static/js/runtime-main.85bd6cb2.js"
+  }
+]);
